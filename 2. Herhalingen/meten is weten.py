4b4bfec1d21a7fdb37864ad98d0517f050154714
yuievv <99083027@mydavinci.nl>
2025-04-12 21:26:42 +0200

    elif-statement

--- 2. Herhalingen/meten is weten.py
+++ 2. Herhalingen/meten is weten.py
@@ -6,3 +6,7 @@ b = int(input("Voer het tweede getal (b) in: "))
 if a > b:
     Max = a
     print(f'a is het grootste getal: {Max}')
+# Bepaal of a kleiner is dan b
+elif a < b:
+    Min = a
+    print(f'a is het kleinste getal: {Min}')
