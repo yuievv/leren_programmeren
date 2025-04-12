39f1fdf13e786e2e5e69732e44b043cdfbcf99d2
yuievv <99083027@mydavinci.nl>
2025-04-12 22:19:00 +0200

    fix

--- 1. De Basis/003 - Foute pizza met uitzonderingen/exceptional_bugs.py
+++ 1. De Basis/003 - Foute pizza met uitzonderingen/exceptional_bugs.py
@@ -1,17 +1,16 @@
 import random
 
-#selecteer 2 nummers
-num1 = str(random.randint(1,10))
-num2 = str(random.randint(5,15))
+num1 = random.randint(1, 10)
+num2 = random.randint(5, 15)
 
-#vraag om een antwoord
-number = input('Weet jij wat '+num1+'+'+num2+' is? ') 
+number = input('Weet jij wat ' + str(num1) + '+' + str(num2) + ' is? ')
 
-#geef reactie op het antwoord
 try:
-    if int(number == num1-num2):
+    number = int(number)
+
+    if number == num1 + num2:
         print('Dat is juist')
-    elif int(number != num1-num2):
+    else:
         print('Nee dat klopt niet')
-    except:
-        print('Dat is geen nummer!')
+except ValueError:
+    print('Dat is geen nummer!')
