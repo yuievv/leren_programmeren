c787870a533e633357771f272441af3fb7c8afe6
yuievv <99083027@mydavinci.nl>
2025-04-12 21:27:41 +0200

    min en max

--- 2. Herhalingen/meten is weten.py
+++ 2. Herhalingen/meten is weten.py
@@ -5,11 +5,19 @@ b = int(input("Voer het tweede getal (b) in: "))
 # Bepaal of a groter is dan b
 if a > b:
     Max = a
+    Min = b
     print(f'a is het grootste getal: {Max}')
 # Bepaal of a kleiner is dan b
 elif a < b:
     Min = a
+    Max = b
     print(f'a is het kleinste getal: {Min}')
 # Anders zijn ze gelijk
 else:
+    Min = a
+    Max = b
     print('a en b zijn even groot')
+
+# Print altijd het minimum en maximum
+print(f'Het minimum is: {Min}')
+print(f'Het maximum is: {Max}')
