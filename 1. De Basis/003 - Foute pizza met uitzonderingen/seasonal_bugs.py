4c84f380bc1a671a8464e90d722dc5f06dd17e2f
yuievv <99083027@mydavinci.nl>
2025-04-12 22:03:05 +0200

    fix

--- 1. De Basis/003 - Foute pizza met uitzonderingen/seasonal_bugs.py
+++ 1. De Basis/003 - Foute pizza met uitzonderingen/seasonal_bugs.py
@@ -1,18 +1,13 @@
 print('Welk seizoen vind jij het fijnst?', 
-      "A) Lente", 
-      "B) Zomer", 
-      "C) Herfst", 
-      "D) Winter")
-keuze = input('? ')
+      "a) Lente", 
+      "b) Zomer", 
+      "c) Herfst", 
+      "d) Winter")
+gekozen_seizoen = input('? ').lower()
 
-if (gekozen_seizoen == 'a' or 'c'):
-    print('Dus jij vindt een tussenseizoen het fijnst...')
-elif (gekozen_seizoen != 'a', 'c', 'd'):
-    weer_type = 'warm'
-else:
-    weer_type = 'koud'
-
-if len(weer_type) > 0:
-    print(f'Dus jij houd van {weer_type} weer!')
-else:
-    print('Dit is geen te kiezen optie!')
+if gekozen_seizoen == 'a' or gekozen_seizoen == 'c':
+    print('Dus jij vindt het tussenseizoen het fijnst...')
+elif gekozen_seizoen == 'b':
+    print('Dus jij houd van warm weer!')
+elif gekozen_seizoen == 'd':
+    print('Dus jij houd van koud weer!')
