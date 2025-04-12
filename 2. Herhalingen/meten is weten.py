b8e14adaa6ee082c69c1f37b55f1828249ad7315
yuievv <99083027@mydavinci.nl>
2025-04-12 21:26:10 +0200

    input en if-statement

---
+++ 2. Herhalingen/meten is weten.py
@@ -0,0 +1,8 @@
+# Vraag de gebruiker om 2 gehele getallen
+a = int(input("Voer het eerste getal (a) in: "))
+b = int(input("Voer het tweede getal (b) in: "))
+
+# Bepaal of a groter is dan b
+if a > b:
+    Max = a
+    print(f'a is het grootste getal: {Max}')
