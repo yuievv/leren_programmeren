cc3fd66b515614b0b5af9a7ae1dd4db98b8f620a
yuievv <99083027@mydavinci.nl>
2025-04-12 19:02:13 +0200

    feestlunch

---
+++ 1. De Basis/feestlunch.py
@@ -0,0 +1,17 @@
+# Aantal producten + prijzen
+aantal_croissantjes = 17
+prijs_croissantje = 0.39  
+aantal_stokbroden = 2
+prijs_stokbrood = 2.78 
+
+# Kortingsbonnen
+aantal_kortingsbonnen = 3
+korting_per_bon = 0.50  
+
+# Totale kosten berekenen
+totale_kosten = (aantal_croissantjes * prijs_croissantje) + (aantal_stokbroden * prijs_stokbrood)
+totale_korting = aantal_kortingsbonnen * korting_per_bon
+te_betalen_bedrag = totale_kosten - totale_korting
+
+# Resultaat
+print(f"Te betalen: {te_betalen_bedrag:.2f} euro")
