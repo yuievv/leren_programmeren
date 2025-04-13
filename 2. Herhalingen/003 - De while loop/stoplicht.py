9f906a815e05fee8599f739e2feacea7d6a6143e
yuievv <99083027@mydavinci.nl>
2025-04-13 15:44:02 +0200

    Verkeerslicht met eindeloze cyclus van rood, groen en oranje

---
+++ 2. Herhalingen/003 - De while loop/stoplicht.py
@@ -0,0 +1,17 @@
+import time
+
+while True:
+    # Rood (20 seconden)
+    for _ in range(20):
+        print("Rood")
+        time.sleep(1)
+    
+    # Groen (30 seconden)
+    for _ in range(30):
+        print("Groen")
+        time.sleep(1)
+    
+    # Oranje (10 seconden)
+    for _ in range(10):
+        print("Oranje")
+        time.sleep(1)
