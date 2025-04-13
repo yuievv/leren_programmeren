7f7a97bc2a994e9938640ba8eee7fc37b202152b
yuievv <99083027@mydavinci.nl>
2025-04-13 13:56:43 +0200

    Complete dungeon avontuur met puzzels, gevechten en sleutelmechanisme

--- 1. De Basis/Proef Module_1 Dungeonkeeper 1.0/dungeon.py
+++ 1. De Basis/Proef Module_1 Dungeonkeeper 1.0/dungeon.py
@@ -1,8 +1,9 @@
-import time, math
+import time, math, random
 
 player_attack = 1
 player_defense = 0
 player_health = 3
+has_key = False
 
 # === [kamer 1] === #
 print('Door de twee grote deuren loop je een gang binnen.')
@@ -15,21 +16,41 @@ time.sleep(1)
 print('Je stapt door de deur heen en je ziet een standbeeld voor je.')
 print('Het standbeeld heeft een sleutel vast.')
 print('Op zijn borst zit een numpad met de toesten 9 t/m 0.')
-print('Daarboven zie je een som staan 11+15=?')
+
+# Random som genereren
+num1 = random.randint(10, 25)
+num2 = random.randint(-5, 75)
+operator = random.choice(['+', '-', '*'])
+
+# Som berekenen
+if operator == '+':
+    correct_answer = num1 + num2
+elif operator == '-':
+    correct_answer = num1 - num2
+else:  # operator == '*'
+    correct_answer = num1 * num2
+
+print(f'Daarboven zie je een som staan {num1}{operator}{num2}=?')
 antwoord = int(input('Wat toest je in?'))
 
-if antwoord == 26:
-    print('Het stadbeeld laat de sleutel vallen en je pakt het op')
+if antwoord == correct_answer:
+    print('Het standbeeld laat de sleutel vallen en je pakt het op')
+    has_key = True
 else:
     print('Er gebeurt niets....')
 
-print('Je zie een deur achter het standbeeld.')
+print('Je ziet een deur achter het standbeeld.')
 print('')
 time.sleep(1)
 
 # === [kamer 3] === #
-item = 'schild'
-player_defense += 1
+# Random item kiezen
+item = random.choice(['schild', 'zwaard'])
+
+if item == 'schild':
+    player_defense += 1
+else:  # zwaard
+    player_attack += 2
 
 print('Je duwt hem open en stap een hele lange kamer binnen.')
 print(f'In deze kamer staat een tafel met daarop een {item}.')
@@ -47,7 +68,7 @@ print('Je loopt tegen een zombie aan.')
 
 zombie_hit_damage = (zombie_attack - player_defense)
 if zombie_hit_damage <= 0:
-    print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
+    print('Jij hebt een te goede verdediging voor de zombie, hij kan je geen schade doen.')
 else:
     zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
     
@@ -55,6 +76,8 @@ else:
     player_attack_amount = math.ceil(zombie_health / player_hit_damage)
 
     if player_attack_amount < zombie_attack_amount:
+        # Bereken nieuwe health van speler
+        player_health -= player_attack_amount * zombie_hit_damage
         print(f'In {player_attack_amount} rondes versla je de zombie.')
         print(f'Je health is nu {player_health}.')
     else:
@@ -66,5 +89,12 @@ time.sleep(1)
 
 # === [kamer 5] === #
 print('Voorzichtig open je de deur, je wilt niet nog een zombie tegenkomen.')
-print('Tot je verbazig zie je een schatkist in het midden van de kamer staan.')
+print('Tot je verbazing zie je een schatkist in het midden van de kamer staan.')
 print('Je loopt er naartoe.')
+
+if has_key:
+    print('Gebruikend met de sleutel open je de schatkist.')
+    print('Gefeliciteerd! Je hebt de schat gevonden en het spel gewonnen!')
+else:
+    print('Je probeert de schatkist te openen maar hij zit op slot.')
+    print('Zonder de sleutel kun je hem niet openen. Game over.')
