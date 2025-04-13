f1b8a000653286290ecce8b073c30ebcef1e39f9
yuievv <99083027@mydavinci.nl>
2025-04-13 14:35:30 +0200

    Voeg aftelmechanisme toe met en zonder vertraging

---
+++ 2. Herhalingen/001 - De for loop/blastoff.py
@@ -0,0 +1,13 @@
+# #terugtellen met pauze van 0.5 seconde:
+ 
+import time
+for i in range(30, 0, -1):
+    print(i)
+    time.sleep(0.5)  
+print("Raket gelanceerd!")
+
+#terugtellen zonder pauze:
+
+# for i in range(30, 0, -1):
+#     print(i)
+# print("Raket gelanceerd!")
