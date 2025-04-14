6a0ff6aebd7db736af741162164f61f051515bdd
yuievv <99083027@mydavinci.nl>
2025-04-14 14:05:01 +0200

    Nu ook de woonplaats

--- 4. De functie van functies/001 - Goed omgaan met functions/naam en leeftijd.py
+++ 4. De functie van functies/001 - Goed omgaan met functions/naam en leeftijd.py
@@ -1,22 +1,23 @@
-def vraag_naam_en_leeftijd():
-    """Vraagt de gebruiker om naam en leeftijd en retourneert deze als dictionary"""
+def vraag_naam_leeftijd_en_woonplaats():
+    """Vraagt de gebruiker om naam, leeftijd en woonplaats en retourneert deze als dictionary"""
     naam = input("Voer uw naam in: ")
     leeftijd = input("Voer uw leeftijd in: ")
-    return {'name': naam, 'age': leeftijd}
+    woonplaats = input("Voer uw woonplaats in: ")
+    return {'name': naam, 'age': leeftijd, 'city': woonplaats}
 
 def verzamel_gegevens():
-    """Verzamelt meerdere namen en leeftijden tot de gebruiker 'stop' invoert"""
+    """Verzamelt meerdere personen tot de gebruiker 'stop' invoert"""
     personen = []
     while True:
         stop_input = input("Toets enter om door te gaan of 'stop' om te printen: ")
         if stop_input.lower() == 'stop':
             break
         
-        persoon = vraag_naam_en_leeftijd()
+        persoon = vraag_naam_leeftijd_en_woonplaats()
         personen.append(persoon)
     return personen
 
 alle_gegevens = verzamel_gegevens()
 
 for persoon in alle_gegevens:
-    print(f"{persoon['name']} is {persoon['age']} jaar")
+    print(f"{persoon['name']}, die in {persoon['city']} woont, is {persoon['age']} jaar")
