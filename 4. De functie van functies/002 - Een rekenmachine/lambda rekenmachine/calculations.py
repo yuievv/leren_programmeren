2c9a08ebc81624496a0f3becd209bf8b5a1d2660
yuievv <99083027@mydavinci.nl>
2025-04-14 15:17:44 +0200

    Lambda dictionary toegevoegd

---
+++ 4. De functie van functies/002 - Een rekenmachine/lambda rekenmachine/calculations.py
@@ -0,0 +1,6 @@
+calculations = {
+    'addition': lambda n1, n2: n1 + n2,
+    'subtraction': lambda n1, n2: n1 - n2,
+    'multiplication': lambda n1, n2: n1 * n2,
+    'division': lambda n1, n2: n1 / n2 if n2 != 0 else None
+}
