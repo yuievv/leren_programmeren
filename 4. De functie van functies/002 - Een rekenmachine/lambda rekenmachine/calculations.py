f8be863f30b0eb4f50deed38c3989a4db060e3a0
yuievv <99083027@mydavinci.nl>
2026-05-28 15:09:41 +0200

    redo: created lambda calculator structure

--- 4. De functie van functies/002 - Een rekenmachine/lambda rekenmachine/calculations.py
+++ 4. De functie van functies/002 - Een rekenmachine/lambda rekenmachine/calculations.py
@@ -1,6 +1,3 @@
-calculations = {
-    'addition': lambda n1, n2: n1 + n2,
-    'subtraction': lambda n1, n2: n1 - n2,
-    'multiplication': lambda n1, n2: n1 * n2,
-    'division': lambda n1, n2: n1 / n2 if n2 != 0 else None
+CALCULATIONS = {
+ 
 }
